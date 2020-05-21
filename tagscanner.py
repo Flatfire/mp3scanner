266858cd82e13d6cb48ceecb91140cfe311682d5
Flatfire <gsavage13@hotmail.ca>
2020-05-21 16:17:36 -0400

    Implemented recursive function for file search

--- tagscanner.py
+++ tagscanner.py
@@ -1,2 +1,25 @@
 import eyed3
+import os
 
+def scanner(directory):
+    ndir = ""
+    print(directory)
+    if (directory == ""):
+        return
+    else:
+        # Recursively search for folders and files
+        # When an MP3 is found, extract tag information
+        os.chdir(directory)
+        dirlist = os.scandir()
+        for i in dirlist:
+            if (i.is_dir()):
+                print(i.path)
+                ndir = os.path.abspath(i.path)
+                scanner(ndir)
+            elif (i.is_file()):
+                if os.path.splitext(i.name)[1] == ".mp3":
+                    print(i.name)
+            os.chdir(directory)
+        dirlist.close()
+        ndir = ""
+        scanner(ndir)
